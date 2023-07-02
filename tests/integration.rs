c7597c68f058ddb6652c4676a8bb9593c5cbe21c
Logan Payton <Logan.Payton@javs.com>
2023-07-02 13:41:48 -0500

    Move test data

--- tests/integration.rs
+++ tests/integration.rs
@@ -2,70 +2,70 @@ use aoc_2022::*;
 
 #[test]
 fn day1_run1() {
-    let input = include_str!("../test_input/day1.txt");
+    let input = include_str!("data/day1.txt");
     let result = day1::run1(input);
     assert_eq!(result, "24000");
 }
 
 #[test]
 fn day1_run2() {
-    let input = include_str!("../test_input/day1.txt");
+    let input = include_str!("data/day1.txt");
     let result = day1::run2(input);
     assert_eq!(result, "45000");
 }
 
 #[test]
 fn day2_run1() {
-    let input = include_str!("../test_input/day2.txt");
+    let input = include_str!("data/day2.txt");
     let result = day2::run1(input);
     assert_eq!(result, "15");
 }
 
 #[test]
 fn day2_run2() {
-    let input = include_str!("../test_input/day2.txt");
+    let input = include_str!("data/day2.txt");
     let result = day2::run2(input);
     assert_eq!(result, "12");
 }
 
 #[test]
 fn day3_run1() {
-    let input = include_str!("../test_input/day3.txt");
+    let input = include_str!("data/day3.txt");
     let result = day3::run1(input);
     assert_eq!(result, "157");
 }
 
 #[test]
 fn day3_run2() {
-    let input = include_str!("../test_input/day3.txt");
+    let input = include_str!("data/day3.txt");
     let result = day3::run2(input);
     assert_eq!(result, "70");
 }
 
 #[test]
 fn day4_run1() {
-    let input = include_str!("../test_input/day4.txt");
+    let input = include_str!("data/day4.txt");
     let result = day4::run1(input);
     assert_eq!(result, "2");
 }
 
 #[test]
 fn day4_run2() {
-    let input = include_str!("../test_input/day4.txt");
+    let input = include_str!("data/day4.txt");
     let result = day4::run2(input);
     assert_eq!(result, "4");
 }
 
 #[test]
 fn day5_run1() {
-    let input = include_str!("../test_input/day5.txt");
+    let input = include_str!("data/day5.txt");
     let result = day5::run1(input);
     assert_eq!(result, "CMZ");
 }
 
 #[test]
 fn day5_run2() {
-    let input = include_str!("../test_input/day5.txt");
+    let input = include_str!("data/day5.txt");
     let result = day5::run2(input);
     assert_eq!(result, "MCD");
 }
